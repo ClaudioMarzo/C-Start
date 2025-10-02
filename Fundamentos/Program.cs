5a9a91e609b463968a22174cc1833493739cd215
Claudio Santarem <claudiosantarem@bemol.com.br>
2025-10-01 20:53:46 -0400

    Variaveis e I/O

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,6 +1,76 @@
-﻿#region Meu primeiro programa
+﻿// #region Meu primeiro Programa em C#
+//  Console.WriteLine("Hello, World!");
+// #endregion
 
-Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+#region Tipos de Váriaveis
+
+// Inserir Dados dentro de uma variavel
+
+Console.WriteLine("Digite seu nome: "); //Imprimir Mensagem
+string nomeInput = Console.ReadLine()!; // Ler Dados do Usuário
+Console.Write($"Seu nome é {nomeInput}"); // Imprimir Dados do Usuário
+
+// Texto (conjunto de caracteres)
+// string nome = "Claudio"; // Exemplo: "João", "Maria", "Carlos"
+// int numero = 10;
+// Console.WriteLine($"Seu nome é {nome}"); //Imprimir Mensagem
+// Console.WriteLine("Seu nome é {0} e {1}", nome, numero); //Imprimir Mensagem
+// // Sintaxe de Declaração de Variáveis do tipo Inteiro que rege número não fracionários
+// // Exemplo: 10, 25, 30, 1000, -50
+// int idade; // posso começar declarando a variável sem atribuir um valor
+// int idade02 = 30; // posso declarar e atribuir um valor na mesma linha
+
+// // Número quebrado (número com parte fracionária)
+// double peso; // Exemplo: 10.5, 25.0, 30.14, 1000.99, -50.75
+// double altura = 1.75; // Exemplo: 1.5, 2.0, 3.14, -0.75
+// double altura02 = 1;
+
+// // Booleano (verdadeiro ou falso)
+// bool estudante; // Exemplo: true (verdadeiro), false (falso)
+// bool estudante02 = true;
+// bool estudante03 = false;
+// bool valor = 2 < 3;
 
 #endregion
 
+
+
+
+
+
+
+
+// #region Tipos de Váriáveis em C# #02
+// // Tipos de Variáveis em C#
+// int idade = 25; // Inteiro
+// double altura = 1.75; // Ponto flutuante
+// string nome = "João"; // Texto
+// bool estudante = true; // Booleano
+// char letra = 'A'; // Caractere
+
+// #endregion
+
+// #region Leitura de Dados #02
+
+// Console.WriteLine("Digite seu nome: "); //Imprimir Mensagem
+// string nomeInput = Console.ReadLine()!; // Ler Dados do Usuário
+// Console.Write($"Seu nome é {nomeInput}"); // Imprimir Dados do Usuário
+
+// #endregion
+
+// #region Operadores Aritméticos #02
+
+// //Somar Dois Número
+
+
+// #endregion
+
+// #region 
+
+// #endregion
